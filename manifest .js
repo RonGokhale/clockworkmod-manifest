6788e28f801f21afaa248c89d92bf34bb4ec889d
RonGokhle <cips173@gmail.com>
2012-08-20 05:40:24 +0530

    1st test
    Change-Id: Id2a056ad2325e2f86e3a251dce22add5f470aaf7

---
+++ manifest .js
@@ -0,0 +1,15 @@
+{
+    "version": "3",
+    "homepage": "http://forum.xda-developers.com/showthread.php?t=1768895",
+    "donate": "http://forum.xda-developers.com/donatetome.php?u=4492528",
+    "roms": [
+        {
+            "device": "p350",
+            "name": "clockworkmod recovery",
+            "modversion": "6.0.1.0",
+            "url": "",
+            "product": "clockworkmod",
+            "summary": "Latest Release",
+        },
+	]
+}
