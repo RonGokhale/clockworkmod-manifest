1574a2a35b706a9b0a786b5fb5de6533a5583adf
RonGokhle <cips173@gmail.com>
2012-08-20 05:45:21 +0530

    add url
    Change-Id: I074f1bfbde06127d1bdad87ffc973f530405d7ab

--- manifest .js
+++ manifest .js
@@ -7,7 +7,7 @@
             "device": "p350",
             "name": "clockworkmod recovery",
             "modversion": "6.0.1.0",
-            "url": "",
+            "url": "https://github.com/downloads/RonGokhle/clockworkmod-manifest/ClockWorkMod_Recovery_V6.0.1.0_Pecan_Build_3_Standard.zip",
             "product": "clockworkmod",
             "summary": "Latest Release",
         },
